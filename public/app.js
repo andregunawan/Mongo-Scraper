200cef1e5906faf25a2b4bbba9ba2aa9f8ae5bf6
Andre A Gunawan <andregunawan@users.noreply.github.com>
2018-01-09 13:28:26 -0800

    first commit

---
+++ public/app.js
@@ -0,0 +1,138 @@
+function createList(data) {
+    $("#news-list").append("<div class='row' style='padding:20px; border:1px solid black'>" +
+        "<div class='col-lg-12'>" +
+        "<div class='row'>" +
+        "<div class='col-lg-9'>" +
+        "<h2>"+data.title+"</h2>" +
+        "</div>" +
+        "<div class='col-lg-3' style='margin-top: 25px'>" +
+        "<button class='btn btn-success' style='font-size: 18px'>Save Article</button>" +
+        "</div>" +
+        "</div>" +
+        "<div class='row'>" +
+        "<div class='col-lg-12'>" +
+        "<p>"+data.desc+"</p>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</div>")
+}
+
+function emptyList(){
+    $("#news-list").append("<div class='row' style='padding:20px'>" +
+        "<div class='col-lg-12' style='text-align: center'>" +
+        "<h3>No news to be shown</h3>" +
+        "</div>" +
+        "</div>");
+}
+
+// We'll be rewriting the table's data frequently, so let's make our code more DRY
+// by writing a function that takes in data (JSON) and creates a table body
+function displayNews(data) {
+  // Add to the table here...
+    if(data.length>0)
+    {
+        for(let i=0;i<data.length;i++)
+        {
+            createList(data[i]);
+        }
+    }
+    else
+    {
+        emptyList();
+    }
+}
+
+$("#myModal").modal("hide");
+$(".articlesNoteModal").modal("hide");
+var newsId;
+
+// $.getJSON("/all", function(data) {
+//   // Call our function to generate a table body
+//   $("#news-list").empty();
+//   displayNews(data);
+// });
+
+$("#homeBtn").on("click", function () {
+    // $.getJSON("/all", function(data) {
+    //     // Call our function to generate a table body
+    //     $("#news-list").empty();
+    //     displayNews(data);
+    // });
+
+    $.getJSON("/all", function(data) {
+        console.log(data);
+        setTimeout(function () {
+            window.location = "/all";
+        }, 1500);
+    })
+
+});
+
+$("#scrapeBtn").on("click", function () {
+    $("#myModal").modal("show");
+    $.ajax({
+        method: "GET",
+        url: "/scrape",
+    }).done(function(data) {
+        console.log(data);
+        setTimeout(function () {
+            window.location = "/all";
+        }, 1500);
+    })
+});
+
+$("#savedArtBtn").on("click", function () {
+    $.getJSON("/saved", function(data) {
+        // Call our function to generate a table body
+        $("#news-list").empty();
+        displayNews(data);
+    });
+});
+
+$(".addNotesBtn").on("click", function () {
+    $(".modal-body").empty();
+    $(".notesTextarea").val("");
+    newsId = $(this).attr("data-id");
+    $.ajax({
+        method: "GET",
+        url: "/addNotes/" + newsId,
+    }).done(function(data) {
+        console.log(data);
+        $(".newsId").text(data.id);
+        if(data.notes.length>0)
+        {
+            for(let i=0;i<data.notes.length;i++)
+            {
+                $(".modal-body").append("<div class='row' style='border:2px solid grey; border-radius: 5px; margin: 0px 5px 5px 5px; padding: 15px'>" +
+                "<div class='col-lg-12'>" +
+                "<div class='col-lg-10'>" +
+                "<p style='margin-top: 7px'>"+data.notes[i].notes+"</p>" +
+                "</div>" +
+                "<div class='col-lg-2'>" +
+                "<button class='btn btn-danger'>X</button>" +
+                "</div>" +
+                "</div>" +
+                "</div>");
+            }
+        }
+        else
+        {
+            $(".modal-body").append("<div class='row' style='border:2px solid grey; border-radius: 5px; margin-left: 5px; margin-right: 5px; padding: 15px'>" +
+            "<div class='col-lg-12' style='text-align: center'>" +
+            "<p style='margin-top: 7px'>No notes to be shown</p>" +
+            "</div></div>")
+        }
+        $(".articlesNoteModal").modal("show");
+    })
+});
+
+$(".saveNotesBtn").on("click", function () {
+    var notesText = $(".notesTextarea").val();
+    $.ajax({
+        method: "POST",
+        url: "/insertNotes?id=" + newsId + "&notes=" + notesText,
+    }).done(function(data) {
+        console.log(data);
+    })
+});
---
+++ server.js
@@ -0,0 +1,112 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
+
+// Initialize Express
+var app = express();
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Database configuration
+var databaseUrl = "new_york_times";
+var collections = ["news"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+    db.news.find({}, function(error, found) {
+        var allArticlesObj = {
+            articles: found
+        };
+        console.log(allArticlesObj);
+        res.render("main", allArticlesObj);
+    });
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+    db.news.find({}, function(error, found) {
+        var allArticlesObj = {
+            articles: found
+        };
+        console.log(allArticlesObj);
+        res.render("home", allArticlesObj);
+    });
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+  // Make a request for the news section of nyt
+  request("https://www.nytimes.com/section/sports?module=SectionsNav&action=click&version=BrowseTree&region=TopBar&contentCollection=Sports&pgtype=sectionfront", function(error, response, html) {
+
+      var $ = cheerio.load(html);
+
+      var results = [];
+      var limit = 0;
+
+      $(".story-meta").each(function(i, element) {
+
+          var title = $(element).children(".headline").text().replace("\n", "").replace("                    ", "").replace("                ", "");
+          var desc = $(element).children(".summary").text();
+
+          db.news.insert({
+              title: title,
+              desc: desc,
+              saved: false
+          });
+          limit++;
+          if(limit === 10) return false;
+      });
+
+      // Log the results once you've looped through each of the elements found with cheerio
+      res.send("Scrape Complete");
+  });
+});
+
+app.get("/saved", function(req, res) {
+    db.news.find({saved:true}, function(error, found) {
+        var allArticlesObj = {
+            articles: found
+        };
+        console.log(allArticlesObj);
+        res.render("saved_articles", allArticlesObj);
+    });
+});
+
+app.get("/addNotes/:id", function(req, res) {
+    db.comments.find({newsId:req.params.id}, function(error, found) {
+        var notesObj = {
+            id: req.params.id,
+            notes: found
+        };
+        console.log(notesObj);
+        res.send(notesObj);
+    });
+});
+
+app.post("/insertNotes", function(req, res) {
+    var id = req.query.id;
+    var notes = req.query.notes;
+    db.comments.insert({newsId:id, notes: notes}, function(error, found) {
+        res.send("Insert Success!" + notes);
+    });
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
